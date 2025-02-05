a64bcf3209a296595824a288c4fb2c73380d15df
happypoot300 <happypoot300@gmail.com>
2025-02-06 02:38:16 +0800

    new responsive landing page

--- react-frontend/src/components/DigitalGapApp.jsx
+++ react-frontend/src/components/DigitalGapApp.jsx
@@ -1,9 +1,9 @@
-// LayoutPage.js
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Button } from "react-bootstrap";
+import { Image } from "react-bootstrap";
 import { FaArrowRight } from "react-icons/fa";
 import { LuFacebook } from "react-icons/lu";
 import { SiInstagram } from "react-icons/si";
@@ -15,6 +15,7 @@ import { useNavigate } from "react-router-dom";
 import clsx from "clsx";
 
 import Style from "../css modules/DigitalGapApp.module.css";
+import phone from "../Images/phoneCropped.png";
 
 const DigitalGapApp = () => {
   const navigate = useNavigate();
@@ -24,13 +25,18 @@ const DigitalGapApp = () => {
   };
 
   return (
-    <Container fluid className={clsx(Style.container, "p-0 m-0")}>
+    <Container fluid className="d-flex flex-column min-vh-100 p-0 m-0">
       {/* Top Row: Only two squares (Square 1 and Square 2) */}
       <Row className="p-0 m-0">
         <Col className={Style.logo_container}>
           <div className={Style.logo}></div>
         </Col>
-        <Col className={clsx(Style.background_designTop, "p-0 m-0")}>
+        <Col
+          className={clsx(
+            Style.background_designTop,
+            "align-items-end p-0 m-0"
+          )}
+        >
           <div
             className={clsx(Style.BarShadow, Style.BarTop, Style.orangeTop)}
           ></div>
@@ -44,12 +50,17 @@ const DigitalGapApp = () => {
       </Row>
 
       {/* Second Grouped Row: Left nested grid and right merged square */}
-      <Row className="mb-4 align-items-stretch">
+      <Row className="d-flex flex-grow-1 p-0 m-0">
         {/* Left side: Nested grid */}
-        <Col xs={8}>
+        <Col
+          lg={4}
+          md={6}
+          xs={12}
+          className="d-flex flex-column flex-grow-1 p-0 m-0"
+        >
           {/* Top nested row: Merged rectangle for Square 4 and 5 */}
-          <Row className="mb-4">
-            <Col className={clsx(Style.quote_container, "p-0")}>
+          <Row className="p-0 pt-4 pb-5 m-0">
+            <Col className="d-flex flex-column align-items-center p">
               <div className={Style.quote}>
                 {"\u00A0"} "Bridging the Digital Gap"
               </div>
@@ -64,9 +75,14 @@ const DigitalGapApp = () => {
               </Button>
             </Col>
           </Row>
-          {/* Bottom nested row: Two squares for Square 7 and 8 */}
-          <Row className={clsx(Style.footer_container, "p-0 m-0")}>
-            <Col className={clsx(Style.background_designBot, "p-0 m-0")}>
+
+          <Row xs={9} className="flex-grow-1 p-0 m-0">
+            <Col
+              className={clsx(
+                Style.background_designBot,
+                "justify-content-end p-0 m-0"
+              )}
+            >
               <div
                 className={clsx(Style.BarShadow, Style.BarBot, Style.orangeBot)}
               ></div>
@@ -78,9 +94,14 @@ const DigitalGapApp = () => {
               ></div>
             </Col>
 
-            <Col className={clsx(Style.followUs_container, "p-0")}>
+            <Col
+              className={clsx(
+                Style.followUs_container,
+                "d-flex flex-column justify-content-end ps-3 m-0"
+              )}
+            >
               <p className={clsx(Style.followUs, "p-0 m-0")}>Follow Us On:</p>
-              <div className={clsx(Style.social_icons, "pb-3 m-0")}>
+              <div className={clsx(Style.social_icons, "ps-2 pb-5 m-0")}>
                 <a
                   className="p-0"
                   href="https://www.facebook.com/"
@@ -118,8 +139,19 @@ const DigitalGapApp = () => {
           </Row>
         </Col>
         {/* Right side: Merged square for Square 6/9 */}
-        <Col lg={3} className={clsx(Style.phone, "d-none d-lg-block")}>
-          <div className={clsx(Style.phone_display, "p-0 m-0")}></div>
+        <Col
+          lg={5}
+          md={6}
+          className="d-none d-md-block align-self-end p-0 m-0 d-flex"
+        >
+          <Container
+            className={clsx(
+              Style.phone_display,
+              "d-flex justify-content-center align-items-center"
+            )}
+          >
+            <Image src={phone} fluid alt="Phone Display" className="p-0 m-0" />
+          </Container>
         </Col>
       </Row>
     </Container>
