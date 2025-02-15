64639309d3eb7e0fa7ec123067c7d668faa0a79f
happypoot300 <happypoot300@gmail.com>
2025-02-16 02:04:30 +0800

    added need help new tutorial

--- react-frontend/src/components/HelpButton.jsx
+++ react-frontend/src/components/HelpButton.jsx
@@ -1,12 +1,87 @@
 // src/HelpButton.js
-import React from "react";
+import React, { useState } from "react";
+import Joyride from "react-joyride";
 import { useLocation } from "react-router-dom";
 import { TiMessages } from "react-icons/ti";
+import { Button } from "react-bootstrap";
+
+//import gifs
+import getstarted_step from "../Images/NeedHelp/getstarted_step.gif";
 
 const HelpButton = ({ openModal }) => {
-  const location = useLocation(); // This hook should now work properly within Router context
+  const location = useLocation();
+  const [runTour, setRunTour] = useState(false);
+  const [showButton, setShowButton] = useState(true);
 
-  // Function to return position based on current route
+  const getTourSteps = () => {
+    switch (location.pathname) {
+      case "/":
+        return [
+          {
+            target: "#getstarted_btn", // CSS selector for the element to highlight
+            content: (
+              <div style={{ maxHeight: "80vh", overflowY: "auto" }}>
+                This is your GET STARTED button. Click it to go to the next
+                page.
+                <img
+                  src={getstarted_step}
+                  style={{ height: "auto", width: "100%" }}
+                />
+              </div>
+            ),
+            placement: "right",
+          },
+          {
+            target: "#learnmore_link",
+            content:
+              "This is the LEARN MORE link. Click it to discover additional details about our service.",
+            placement: "right",
+          },
+          {
+            target: "#aboutus_section",
+            content:
+              "Here in the ABOUT US section, learn more about our mission, values, and what we do.",
+            placement: "top",
+          },
+          {
+            target: "#development_team",
+            content:
+              "Meet our DEVELOPMENT TEAM. These are the dedicated professionals behind our platform.",
+            placement: "top",
+          },
+          {
+            target: "#followus",
+            content:
+              "Follow us on social media! Stay updated by connecting with us on Facebook, Instagram, X, and YouTube.",
+            placement: "top",
+          },
+        ];
+      case "/connect-to-phone":
+        return [
+          {
+            target: ".connect-button", // CSS selector for the element to highlight
+            content: "Click this button to connect to your phone.",
+            placement: "top",
+          },
+        ];
+      // Add more cases for other routes
+      default:
+        return [];
+    }
+  };
+
+  const handleHelpClick = () => {
+    setRunTour(true);
+    setShowButton(false);
+
+    const targetElement = document.querySelector(getTourSteps()[0].target);
+    if (targetElement) {
+      const rect = targetElement.getBoundingClientRect();
+      const offset = rect.top + rect.height / 2 - window.innerHeight / 2;
+      window.scrollTo({ top: offset, behavior: "smooth" });
+    }
+  };
+
   const getHelpButtonPosition = () => {
     switch (location.pathname) {
       case "/connect-to-phone":
@@ -21,9 +96,60 @@ const HelpButton = ({ openModal }) => {
   };
 
   return (
-    <div className="help-button" onClick={openModal} style={getHelpButtonPosition()}>
-      <TiMessages size={30} className="icon" />
-      <p>Need Help?</p>
+    <div>
+      {/* Help Button */}
+      {showButton && (
+        <Button
+          className="help-button"
+          onClick={handleHelpClick}
+          style={getHelpButtonPosition()}
+        >
+          <TiMessages size={30} className="icon" />
+          <p>Need Help?</p>
+        </Button>
+      )}
+
+      {/* Joyride component for guided tour */}
+      <Joyride
+        steps={getTourSteps()}
+        run={runTour}
+        continuous={true}
+        showSkipButton={true}
+        callback={(data) => {
+          if (
+            data.action === "close" ||
+            data.status === "finished" ||
+            data.status === "skipped"
+          ) {
+            setRunTour(false);
+            setShowButton(true);
+          }
+        }}
+        styles={{
+          tooltip: {
+            width: "65vw",
+            padding: "20px",
+            fontSize: "25px",
+            fontWeight: "700",
+            fontFamily: "Big Shoulders Display",
+          },
+          buttonNext: {
+            fontSize: "30px !important",
+            fontWeight: "700",
+            fontFamily: "Big Shoulders Display !important",
+          },
+          buttonBack: {
+            fontSize: "30px !important",
+            fontWeight: "700",
+            fontFamily: "Big Shoulders Display !important",
+          },
+          buttonSkip: {
+            fontSize: "30px !important",
+            fontWeight: "700",
+            fontFamily: "Big Shoulders Display !important",
+          },
+        }}
+      />
     </div>
   );
 };
--- react-frontend/src/components/LandingPage.jsx
+++ react-frontend/src/components/LandingPage.jsx
@@ -44,51 +44,97 @@ const LandingPage = () => {
         <Col className={Style.logo_container}>
           <div className={Style.logo}></div>
         </Col>
-        <Col className={clsx(Style.background_designTop, "align-items-end p-0 m-0")}>
-          <div className={clsx(Style.BarShadow, Style.BarTop, Style.orangeTop)}></div>
-          <div className={clsx(Style.BarShadow, Style.BarTop, Style.blueTop)}></div>
-          <div className={clsx(Style.BarShadow, Style.BarTop, Style.yellowTop)}></div>
+        <Col
+          className={clsx(
+            Style.background_designTop,
+            "align-items-end p-0 m-0"
+          )}
+        >
+          <div
+            className={clsx(Style.BarShadow, Style.BarTop, Style.orangeTop)}
+          ></div>
+          <div
+            className={clsx(Style.BarShadow, Style.BarTop, Style.blueTop)}
+          ></div>
+          <div
+            className={clsx(Style.BarShadow, Style.BarTop, Style.yellowTop)}
+          ></div>
         </Col>
       </Row>
 
       {/* Second Row - Nested Layout */}
       <Row className="d-flex p-0 m-0">
         {/* Left side with 3 stacked rows */}
-        <Col lg={4} md={6} xs={12} className="d-flex flex-column flex-grow-1 p-0 m-0">
+        <Col
+          lg={4}
+          md={6}
+          xs={12}
+          className="d-flex flex-column flex-grow-1 p-0 m-0"
+        >
           <Row className="d-flex p-0 m-0">
             <Col className="d-flex flex-column">
               <div className={Style.quote}>
                 {"\u00A0"} "Bridging the Digital Gap"
               </div>
-              <Button className={Style.getStarted} type="button" onClick={handleGetStartedClick}>
+              <Button
+                className={Style.getStarted}
+                type="button"
+                onClick={handleGetStartedClick}
+                id="getstarted_btn"
+              >
                 GET STARTED
                 <FaArrowRight className={Style.arrowRight} />
               </Button>
-              <div className={Style.learnMoreText_container} >
-                <p className={Style.learnMoreText} onClick={handleLearnMoreClick}>Learn More</p>
+              <div className={Style.learnMoreText_container}>
+                <p
+                  className={Style.learnMoreText}
+                  onClick={handleLearnMoreClick}
+                  id="learnmore_link"
+                >
+                  Learn More
+                </p>
               </div>
             </Col>
           </Row>
           <Row className="d-flex p-0 m-0">
-            <Col className={clsx(Style.background_designBot, "justify-content-end p-0 m-0")}>
-              <div className={clsx(Style.BarShadow, Style.BarBot, Style.orangeBot)}></div>
-              <div className={clsx(Style.BarShadow, Style.BarBot, Style.blueBot)}></div>
-              <div className={clsx(Style.BarShadow, Style.BarBot, Style.yellowBot)}></div>
+            <Col
+              className={clsx(
+                Style.background_designBot,
+                "justify-content-end p-0 m-0"
+              )}
+            >
+              <div
+                className={clsx(Style.BarShadow, Style.BarBot, Style.orangeBot)}
+              ></div>
+              <div
+                className={clsx(Style.BarShadow, Style.BarBot, Style.blueBot)}
+              ></div>
+              <div
+                className={clsx(Style.BarShadow, Style.BarBot, Style.yellowBot)}
+              ></div>
             </Col>
           </Row>
           <Row className="d-flex p-0 m-0">
-            <Col md={6} className={clsx(Style.aboutUs_container, "d-flex p-0 m-0")} ref={aboutUsRef}>
-              <p className={Style.aboutUsText}>
+            <Col
+              md={6}
+              className={clsx(Style.aboutUs_container, "d-flex p-0 m-0")}
+              ref={aboutUsRef}
+              id="aboutus_section"
+            >
+              <p className={Style.aboutUsText} id="aboutus_section">
                 About Us
               </p>
-              <p className={Style.aboutUsTextP}>
-                DIGILIT is a learning platform designed to help middle-aged and elderly users confidently navigate their 
-                smartphones. We believe technology should be accessible to everyone, regardless of experience. 
-                Through easy-to-follow lessons with visual guides, we simplify essential smartphone skills.
+              <p className={Style.aboutUsTextP} id="aboutus_section">
+                DIGILIT is a learning platform designed to help middle-aged and
+                elderly users confidently navigate their smartphones. We believe
+                technology should be accessible to everyone, regardless of
+                experience. Through easy-to-follow lessons with visual guides,
+                we simplify essential smartphone skills.
                 <br /> <br />
-                Our goal is to empower users with the knowledge they need to stay connected, informed, and independent in today’s 
-                digital world. Whether you're just starting or looking to improve your skills, DIGILIT is here to guide 
-                you every step of the way.
+                Our goal is to empower users with the knowledge they need to
+                stay connected, informed, and independent in today’s digital
+                world. Whether you're just starting or looking to improve your
+                skills, DIGILIT is here to guide you every step of the way.
               </p>
             </Col>
           </Row>
@@ -97,35 +143,54 @@ const LandingPage = () => {
         {/* Right side - Single merged row */}
         <Col md={6} className={clsx(Style.phone_container, "p-0 m-0")}>
           <div>
-            <Image src={phone} fluid alt="Phone Display" className={Style.phone} draggable="false" />
+            <Image
+              src={phone}
+              fluid
+              alt="Phone Display"
+              className={Style.phone}
+              draggable="false"
+            />
           </div>
         </Col>
       </Row>
 
       {/* Third Row - 2 Equal Columns */}
-      <Row className={"d-flex align-items-center pm-0 m-0"}> 
+      <Row className={"d-flex align-items-center pm-0 m-0"}>
         {/* Development Team Section */}
-        <Col md={6} className={clsx(Style.devTeamSection, "d-flex pm-0 m-0")}>  
+        <Col md={6} className={clsx(Style.devTeamSection, "d-flex pm-0 m-0")}>
           <Col xs={12} className="d-flex align-items-center p-0 mb-0">
             <h6 className={Style.devTeamTitle}>Development Team</h6>
             <div className={Style.devTeamLine}></div>
           </Col>
 
           {/* Team Members - Row for Desktop, Column for Mobile */}
-          <Container xs={12} lg={4} className={clsx(Style.devProfiles_container, "text-center")}> 
+          <Container
+            xs={12}
+            lg={4}
+            className={clsx(Style.devProfiles_container, "text-center")}
+            id="development_team"
+          >
             <Col xs={12} lg={4} className={Style.devProfile}>
               <Image src={linny} roundedCircle className={Style.devPicture} />
               <div className="d-flex flex-column mt-3">
                 <h6 className={Style.devName}>Rexie Ryl Nadela</h6>
-                <p className={Style.devRole}>Student at University of Cabuyao</p>
+                <p className={Style.devRole}>
+                  Student at University of Cabuyao
+                </p>
               </div>
             </Col>
 
             <Col xs={12} lg={4} className={Style.devProfile}>
-              <Image src={mingming} roundedCircle className={Style.devPicture} />
+              <Image
+                src={mingming}
+                roundedCircle
+                className={Style.devPicture}
+              />
               <div className="d-flex flex-column mt-3">
                 <h6 className={Style.devName}>Kerzania Macalde</h6>
-                <p className={Style.devRole}>Student at University of Cabuyao</p>
+                <p className={Style.devRole}>
+                  Student at University of Cabuyao
+                </p>
               </div>
             </Col>
 
@@ -133,14 +198,22 @@ const LandingPage = () => {
               <Image src={tuck} roundedCircle className={Style.devPicture} />
               <div className="d-flex flex-column mt-3">
                 <h6 className={Style.devName}>Michael Saymo</h6>
-                <p className={Style.devRole}>Student at University of Cabuyao</p>
+                <p className={Style.devRole}>
+                  Student at University of Cabuyao
+                </p>
               </div>
             </Col>
           </Container>
         </Col>
 
         {/* Follow Us Section */}
-        <Col className={clsx(Style.followUs_container,"d-flex flex-column p-0 m-0")}>
+        <Col
+          className={clsx(
+            Style.followUs_container,
+            "d-flex flex-column p-0 m-0"
+          )}
+          id="followus"
+        >
           <p className={clsx(Style.followUs)}>Follow Us On:</p>
           <div className={clsx(Style.social_icons, "ps-2 pb-4 m-0")}>
             <a
